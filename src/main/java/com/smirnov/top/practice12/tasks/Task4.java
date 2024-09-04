d4aad80b7e251ae20140803f8e6293eb65d2be04
AlexCher94 <alexeycherepyanov@mail.ru>
2024-09-04 21:25:11 +0700

    fix Task4

--- src/main/java/com/smirnov/top/practice12/tasks/Task4.java
+++ src/main/java/com/smirnov/top/practice12/tasks/Task4.java
@@ -1,9 +1,11 @@
 package com.smirnov.top.practice12.tasks;
 
 public class Task4 {
-    public static int massive(int[] array) {
+    public static int [] massive(int[] array) {
         int[] result = new int [2];
         int inc;
         int dec;
+
+        return result;
     }
 }
